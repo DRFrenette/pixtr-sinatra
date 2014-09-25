efa83c9ac68160cee44fcfb3ddf4d1ec6c42c9af
Dan Frenette <paradox815@gmail.com>
2014-09-25 14:08:47 -0400

    Updated Sinatra Pixtr app, added editing features

--- app.rb
+++ app.rb
@@ -9,6 +9,17 @@ ActiveRecord::Base.establish_connection(
   database: "photo_gallery"
 )
 
+class Gallery < ActiveRecord::Base
+end
+
+class Image < ActiveRecord::Base
+  def secure_url
+    url # convert http to https
+    name
+    description
+  end
+end
+
 # galleries table
 class Gallery < ActiveRecord::Base
 end
@@ -28,20 +39,31 @@ end
 
 post "/galleries" do
   new_gallery_name = params[:gallery][:name]
-  Gallery.create(name: new_gallery_name)
+  gallery = Gallery.create(name: new_gallery_name)
 
-  redirect to("/")
+  # send_file "/galleries/#{params[:file_name]}"
+  redirect to("/galleries/#{gallery.id}")
 end
+
+get "/galleries/:id/edit" do
+  @gallery = Gallery.find(params[:id])
+  erb :edit_gallery
+end
+
+patch "/galleries/:id" do
+  puts params
+  id = params[:id]
+  gallery = Gallery.find(params[:id])
+  gallery.update(params[:gallery])
+  redirect(to("/galleries/#{id}"))
+end
+
 
 get "/galleries/:id" do 
   id = params[:id]
   #   gallery = Gallery.where({id: id, name: "stuff", description: "Cool"})
-  gallery = Gallery.find(id)
-  @name = gallery.name
+  @gallery = Gallery.find(id)
   @images = Image.where(gallery_id: id)
+  
   erb :gallery
-
-
 end
-# end
-#
